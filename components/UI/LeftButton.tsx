0ff2be6b59d934008d54c1a22110d3470d8e32a4
connorklee0 <connorklee0@gmail.com>
2026-02-13 12:30:25 -0600

    create left button component

---
+++ components/UI/LeftButton.tsx
@@ -0,0 +1,31 @@
+import LeftBtn from "@/public/buttin-icon-shrunk-left.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+interface LeftButtonProps {
+  text: string;
+  link: string;
+  disabled?: boolean;
+}
+
+const LeftButton = ({ text, link, disabled = false }: LeftButtonProps) => {
+  return (
+    <Link
+      href={link}
+      className={`flex gap-3 items-center group ${
+        disabled ? "pointer-events-none opacity-50" : ""
+      }`}
+      aria-label={text}
+    >
+      <Image
+        src={LeftBtn}
+        alt=""
+        className="transition-transform duration-300 group-hover:scale-110 size-12"
+        aria-hidden="true"
+      />
+      <span className="text-sm uppercase font-bold">{text}</span>
+    </Link>
+  );
+};
+
+export default LeftButton;
