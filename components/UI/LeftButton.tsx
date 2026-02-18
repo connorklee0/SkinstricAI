7387884e53aa292e264fced78e5853e2d2883a40
connorklee0 <connorklee0@gmail.com>
2026-02-18 12:01:48 -0600

    responsive nav buttons

--- components/UI/LeftButton.tsx
+++ components/UI/LeftButton.tsx
@@ -10,22 +10,39 @@ interface LeftButtonProps {
 
 const LeftButton = ({ text, link, disabled = false }: LeftButtonProps) => {
   return (
-    <Link
-      href={link}
-      className={`flex gap-3 items-center group ${
-        disabled ? "pointer-events-none opacity-50" : ""
-      }`}
-      aria-label={text}
-    >
-      <Image
-        src={LeftBtn}
-        alt=""
-        className="transition-transform duration-300 group-hover:scale-110 size-12"
-        aria-hidden="true"
-        draggable={false}
-      />
-      <span className="text-sm uppercase font-bold">{text}</span>
-    </Link>
+    <>
+      <div className="max-md:hidden">
+        <Link
+          href={link}
+          className={`flex gap-3 items-center group ${
+            disabled ? "pointer-events-none opacity-50" : ""
+          }`}
+          aria-label={text}
+        >
+          <Image
+            src={LeftBtn}
+            alt=""
+            className="transition-transform duration-300 group-hover:scale-110 size-12"
+            aria-hidden="true"
+            draggable={false}
+          />
+          <span className="text-sm uppercase font-bold">{text}</span>
+        </Link>
+      </div>
+
+      <div className="md:hidden relative transition duration-300 hover:scale-110">
+        <div className="w-14 h-14 border-2 border-[#A0A4AB] rotate-45 " />
+        <Link
+          href={link}
+          className={`absolute inset-0 flex items-center justify-center group ${
+            disabled ? "pointer-events-none opacity-50" : ""
+          }`}
+          aria-label={text}
+        >
+          <span className="text-xs uppercase font-bold ">{text}</span>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- components/UI/RightButton.tsx
+++ components/UI/RightButton.tsx
@@ -10,22 +10,39 @@ interface RightButtonProps {
 
 const RightButton = ({ text, link, disabled = false }: RightButtonProps) => {
   return (
-    <Link
-      href={link}
-      className={`flex gap-3 items-center group ${
-        disabled ? "pointer-events-none opacity-50" : ""
-      }`}
-      aria-label={text}
-    >
-      <span className="text-sm uppercase font-bold">{text}</span>
-      <Image
-        src={RightBtn}
-        alt=""
-        className="transition-transform duration-300 group-hover:scale-110 size-12"
-        aria-hidden="true"
-        draggable={false}
-      />
-    </Link>
+    <>
+      <div className="max-md:hidden">
+        <Link
+          href={link}
+          className={`flex gap-3 items-center group ${
+            disabled ? "pointer-events-none opacity-50" : ""
+          }`}
+          aria-label={text}
+        >
+          <span className="text-sm uppercase font-bold">{text}</span>
+          <Image
+            src={RightBtn}
+            alt=""
+            className="transition-transform duration-300 group-hover:scale-110 size-12"
+            aria-hidden="true"
+            draggable={false}
+          />
+        </Link>
+      </div>
+
+      <div className="md:hidden relative transition duration-300 hover:scale-110">
+        <div className="w-14 h-14 border-2 border-[#A0A4AB] rotate-45 " />
+        <Link
+          href={link}
+          className={`absolute inset-0 flex items-center justify-center group ${
+            disabled ? "pointer-events-none opacity-50" : ""
+          }`}
+          aria-label={text}
+        >
+          <span className="text-xs uppercase font-bold ">{text}</span>
+        </Link>
+      </div>
+    </>
   );
 };
 
